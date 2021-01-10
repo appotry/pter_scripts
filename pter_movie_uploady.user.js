82f7d97dd7980796b311cbfe1bfdd6bcda2c521e
scatowl <scatowl@qq.com>
2021-01-10 11:06:12 +0800

    更新0.1.4 新增国语中字选项；美化样式 @qqboy1017

--- pter_movie_uploady.user.js
+++ pter_movie_uploady.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pter Movie Uploady
 // @namespace    https://pterclub.com/forums.php?action=viewtopic&topicid=3391
-// @version      0.1.3
+// @version      0.1.4
 // @description  Auto get movie&TV info from douban&imdb for Pterclub
 // @author       scatking
 // @match        https://pterclub.com/upload.php*
@@ -17,7 +17,6 @@ function fill_form(response) {
     $('#descr').val(data['format']);
     if (data['site'] === 'douban'){
         var trans_titles='',directors='',casts='';
-        console.log(data['foreign_title']);
         if (data['foreign_title'].length == 0){ trans_titles= data['chinese_title']}
         else {
             data.trans_title.forEach(function (trans_title) {
@@ -66,13 +65,23 @@ function triger(url) {
         });
     }
 }
+
+function writeInto(){
+    const subtitle = $('input[name="small_descr"]');
+    const cstext = subtitle.val() + "[国语中字]";
+    subtitle.val(cstext);
+}
+
 (function() {
     'use strict';
     let imdb_url = $('input[name="url"][type="text"]');
     let douban_url = $('input[name="douban"]');
+    let subtitle = $('input[name="small_descr"]');
 
     imdb_url.after('<a href="javascript:;" id="fill_imdb" style="color:green">Auto Fill</a>');
     douban_url.after('<a href="javascript:;" id="fill_douban" style="color:green">Auto Fill</a>');
+    subtitle.after('<a href="javascript:;" id="fill_cs" style="color:green">[国语中字]</a>');
+    $('#fill_cs').click(function () {writeInto()});
     $('#fill_imdb').click(function () {triger(imdb_url.val())});
     $('#fill_douban').click(function () {triger(douban_url.val())})
 })();
